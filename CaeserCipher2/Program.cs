e65b654b9dd5842d89a1d27d96a809167bba46f5
Jennyf19 <ferriesj@gmail.com>
2016-07-25 23:14:46 -0700

    Caesar cipher

--- CaeserCipher2/Program.cs
+++ CaeserCipher2/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,16 @@ namespace CaeserCipher2
     {
         static void Main(string[] args)
         {
+            string alphabet = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Cipher myCipher = new Cipher(alphabet);
+            myCipher.OffSet(5);
+            Console.WriteLine(alphabet);
+            Console.WriteLine(myCipher.encodedAlphabet);
+            string finalMessage = myCipher.Cypher("RYAN IS SUPER AWESOME");
+            Console.WriteLine(finalMessage);
+            string decodeMessage = myCipher.Decypher(finalMessage);
+            Console.WriteLine(decodeMessage);
+            Console.ReadLine();
         }
     }
 }
---
+++ CaeserCipher2/cipher.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaeserCipher2
+{
+    public class Cipher
+
+    {
+        #region Properties
+        //data inside the class is the property
+
+        public string alphabet;
+        public string encodedAlphabet;
+        #endregion
+
+        public Cipher(string Alphabet)
+        {
+            alphabet = Alphabet;
+        }
+
+        #region Methods
+
+        public void OffSet(int num)
+        {
+            string firstPartOfAlphabet = alphabet.Substring(0, num);
+            string secondPartOfAlphabet = alphabet.Substring(num);
+            encodedAlphabet = secondPartOfAlphabet + firstPartOfAlphabet;
+        }
+
+        // TODO: Encode the message from the user.
+        public string Cypher(string message)
+        {
+            string newMessage = String.Empty;
+            for (int i = 0; i < message.Length; i++)
+            {
+                string letter = message.Substring(i, 1);
+                int index = alphabet.IndexOf(letter);
+
+                //TODO: input validation
+                if (index < 0) continue;
+
+                string encodedLetter = encodedAlphabet.Substring(index, 1);
+                newMessage = newMessage + encodedLetter;
+            }
+            return newMessage;
+        }
+
+        //TODO: decypher a string
+        public string Decypher(string encodedMessage)
+        {
+            string decipheredMessage = String.Empty;
+            for (int i = 0; i < encodedMessage.Length; i++)
+            {
+                string encodedLetter = encodedMessage.Substring(i, 1);
+                int index = encodedAlphabet.IndexOf(encodedLetter);
+
+                //TODO: input validation
+                if (index < 0) continue;
+
+                string decipheredLetter = alphabet.Substring(index, 1);
+                decipheredMessage = decipheredMessage + decipheredLetter;
+            }
+            return decipheredMessage;
+        }
+
+        /*public void OffSet(int num)
+        {
+            char[] theNameOfMyArray = input.ToCharArray();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char firstCharacter = theNameOfMyArray[i];
+                int offSetNumber = input.Length - num;
+                theNameOfMyArray[offSetNumber] = firstCharacter;
+                
+                
+                theNameOfMyArray[3] = 'c';
+                char aSingleElementOfMyArray = theNameOfMyArray[12];
+                
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < theNameOfMyArray.Length; i++)
+            {
+                sb.Append(theNameOfMyArray[i]);
+            }
+            input = sb.ToString();
+
+        }*/
+
+        #endregion
+    }
+}
